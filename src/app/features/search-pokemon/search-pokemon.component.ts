12cf700947c9002e15a3d141d72b8f912869153f
unknown <danilo.cavalli@enel.com>
2024-04-19 14:57:35 +0200

    added ng-select

--- src/app/features/search-pokemon/search-pokemon.component.ts
+++ src/app/features/search-pokemon/search-pokemon.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search-pokemon',
   standalone: true,
-  imports: [],
+  imports: [
+    NgSelectModule,
+    FormsModule
+  ],
   templateUrl: './search-pokemon.component.html',
   styleUrl: './search-pokemon.component.scss'
 })
 export class SearchPokemonComponent {
+
+  selectedCar!: number;
+
+  cars = [
+      { id: 1, name: 'Volvo' },
+      { id: 2, name: 'Saab' },
+      { id: 3, name: 'Opel' },
+      { id: 4, name: 'Audi' },
+  ];
 
 }
